cab0c9ac7df38654138d74f7346066eff39ec9fa
arahim2 <arahim2@illinois.edu>
2020-04-05 20:32:17 -0500

    stuff

--- json_formatter copy/write.py
+++ json_formatter copy/write.py
@@ -8,22 +8,26 @@ class Writer:
     def __init__(self):
         Writer.user['portfolio'] = {}
         Writer.user['account_balance'] = 0.0
-        Writer.user['previous_account_values'] = {}
+        Writer.user['previous_account_values'] = []
 
     def update_account_balance(self, balance):
         Writer.user['account_balance'] = balance
 
     def update_previous_account_values(self, value):
-        Writer.user['previous_account_values'][str(round(t.time() * 1000000))] = value
+        to_add = {
+            'time' : str(round(t.time() * 1000000)),
+            'amount' : value
+        }
+        Writer.user['previous_account_values'].append(to_add)
 
     def add_stock(self, name):
-            Writer.user['portfolio']['stock - ' + name] = {
+            Writer.user['portfolio'][name] = {
                 'current_qty' : 0,
                 'transactions' : {}
             }
 
     def add_transaction(self, name, price, quantity):
-        Writer.user['portfolio']['stock: ' + name]['transactions'][str(round(t.time() * 1000000)) + name.upper()] = {
+        Writer.user['portfolio'][name]['transactions'][str(round(t.time() * 1000000)) + name.upper()] = {
             'price' : price,
             'quantity' : quantity,
             'time' : round(t.time() * 1000000)
