594731b11e1a272b1005632d7e7667575f0927ae
skwa <keweis2@illinois.edu>
2020-04-18 19:56:29 -0500

    help function request

--- UI/yourpage.py
+++ UI/yourpage.py
@@ -20,15 +20,15 @@ b1 = tk.Button(window, text='print selection', width=15,
                height=2, command=print_selection)
 b1.pack()
 var2 = tk.StringVar()
-var2.set(('stock a', 'stock b', 'stock c', 'stock d'))
+# var2.set(('stock a', 'stock b', 'stock c', 'stock d'))
 lb = tk.Listbox(window, listvariable=var2)
 # list_items=[1,2,3,4]
 # for item in list_items:
 #   lb.insert('end',item)
-lb.insert(1, 'stock e')
-lb.insert(2, 'stock f')
-lb.insert(3, 'stock g')
-lb.insert(4, 'stock h')
+# lb.insert(1, 'stock e')
+# lb.insert(2, 'stock f')
+# lb.insert(3, 'stock g')
+# lb.insert(4, 'stock h')
 lb.pack()
 
 """""
