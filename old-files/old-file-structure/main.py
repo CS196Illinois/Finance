03dfab9625d314ca65d57346c62769fcdaa25465
Matt Geimer <matasgeimer@gmail.com>
2020-05-03 14:08:07 -0500

    Remove old files

--- old-files/old-file-structure/main.py
+++
@@ -1,5 +0,0 @@
-#This is the main script. It should run any initial set-up, then launch the home page
-
-from models import test
-
-test.foo()
--- old-files/old-file-structure/models/README.md
+++
@@ -1,1 +0,0 @@
-This is where all the python files for gathering data from APIs and JSON files should go
